18e2697e8a153a986aa96de320dcfb0c69ca031d
Abhay kumar <abhayrobotics1908@gmail.com>
2024-08-22 00:38:44 +0530

    custom data type

---
+++ basic/app.ts
@@ -0,0 +1,61 @@
+console.log("Pizza App")
+
+const menu =[
+    {name:"Pepperoni", price: 10},
+    {name:"Hawaiian", price: 9},
+    {name:"Margherita", price: 8},
+
+]
+
+let cashInRegister:number = 100;
+const orderQueue =[];
+let nextOrderId =101;
+
+// Exercise 1: Create addNewPizza function
+const addNewPizza=(name,price) =>{
+
+    menu.push({name,price});
+}
+
+addNewPizza("veg","25")
+console.log(menu)
+
+// Exercise 2: Create placeOrder function that add money in register and add new queue
+
+
+const placeOrder =(item)=>{
+
+    for( let x in menu){
+        console.log(menu[x])
+        if(menu[x].name===item){
+            cashInRegister =cashInRegister+ menu[x].price;
+            order={
+                id:nextOrderId++,
+                pizza: menu[x],
+                status:"ordered"
+            }
+            orderQueue.push(order)
+            return menu[x];
+        }
+    }
+    
+}
+placeOrder("Hawaiian");
+
+console.log(orderQueue,cashInRegister)
+
+// Exercise 3: create complete order
+const completeOrder =(id:number)=>{
+
+    orderQueue.filter((item)=>{
+        if(item.id===id){
+            console.log(item)
+            item.status="Completed";
+            return item
+        };
+    })
+    
+}
+
+completeOrder("101")
+console.log(orderQueue,cashInRegister)
---
+++ basic/basic.ts
@@ -0,0 +1,12 @@
+// corrent en in ts
+let friend ="Raj";
+
+
+// Primitive data type , explicity declaring he variable
+let myName:string = "ABhay";
+
+let myAge: number =30;
+
+let isAlive: boolean = true;
+
+// Custom type
